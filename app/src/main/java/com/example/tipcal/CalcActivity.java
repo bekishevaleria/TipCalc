8573dc0c2b6f811e40774cf6f7e489e408822bb5
bekishevaleria <bekisheva.valeria@yandex.ru>
2020-11-27 20:22:08 +0700

    Добавила переменные и создала экземпляр Calc

--- app/src/main/java/com/example/tipcal/CalcActivity.java
+++ app/src/main/java/com/example/tipcal/CalcActivity.java
@@ -3,8 +3,20 @@ package com.example.tipcal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class CalcActivity extends AppCompatActivity {
+
+    private double amount = 0.0; // Сумма счёта
+    private double percent = 0.15; // Процент чаевых по умолчанию.
+    private EditText et_amount; // Поле для ввода суммы счёта
+    private SeekBar sb_percent; // Ползунок для процентов
+    private TextView tv_percent; // Поле для значения процента
+    private TextView tv_tip; // Поле для суммы чаевых
+    private TextView tv_total; // Поле для итоговой суммы
+    private Calc tipCalc = new Calc();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
